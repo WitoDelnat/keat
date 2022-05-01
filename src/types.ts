c7e1167562bd4ee49409030f528d2b3e112c27a5
Wito Delnat <wito.delnat@gmail.com>
2022-05-01 21:26:52 +0200

    feat: add config setter

--- src/types.ts
+++ src/types.ts
@@ -5,11 +5,12 @@ export type HashFn = (user: User, feature: string) => number; // number between 
 export type Rule = boolean | string | number | (string | number)[];
 export type BiVariateRule = Rule;
 export type MultiVariateRule = Rule[];
+export type Config = Record<string, BiVariateRule | MultiVariateRule>;
 
 export type KeatInit<TFeatures extends RawFeatures> = {
   audiences: Record<string, AudienceFn>;
   features: TFeatures;
-  config: Record<string, BiVariateRule | MultiVariateRule>;
+  config: Config;
   hashFn?: HashFn;
 };
 
