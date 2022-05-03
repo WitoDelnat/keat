3557620d20785fde9f7685d94bd7d49303d292c9
Wito Delnat <wito.delnat@gmail.com>
2022-05-03 22:20:30 +0200

    feat: allow custom user type

--- src/plugins/useAnonymous.ts
+++ src/plugins/useAnonymous.ts
@@ -1,27 +1,30 @@
-import { User } from "../types";
-import { Plugin } from "./plugin";
 import { nanoid } from "nanoid";
+import { Plugin } from "./plugin";
 
 type AnonymousPluginOptions = {
   persist?: boolean;
 };
 
 export const useAnonymous = (options?: AnonymousPluginOptions): Plugin => {
-  let anonymousId;
-  if (options?.persist) {
-    anonymousId = localStorage.getItem("__keat_aid");
-    if (!anonymousId) {
-      anonymousId = nanoid();
-      localStorage.setItem("__keat_aid", anonymousId);
-    }
-  } else {
-    anonymousId = nanoid();
-  }
-
-  const anonymousUser: User = { sub: anonymousId };
+  let anonymousUser: unknown;
 
   return {
-    onEval: (_name, user, { setUser }) => {
+    onPluginInit({ userIdentifier }) {
+      let anonymousId;
+
+      if (options?.persist) {
+        anonymousId = localStorage.getItem("__keat_aid");
+        if (!anonymousId) {
+          anonymousId = nanoid();
+          localStorage.setItem("__keat_aid", anonymousId);
+        }
+      } else {
+        anonymousId = nanoid();
+      }
+
+      anonymousUser = { [userIdentifier]: anonymousId };
+    },
+    onEval(_name, user, { setUser }) {
       if (!user) {
         setUser(anonymousUser);
       }
